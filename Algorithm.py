577271e4f7c1f803d339607df5a17e6321c0c55b
Axieof <pritheev24@gmail.com>
2024-10-15 15:49:46 +0800

    Added Reading csv

--- Algorithm.py
+++ Algorithm.py
@@ -1,1 +1,12 @@
-# test
+# Intro to computational thinking
+# Assignment 1
+# Team 3
+# Tham, Nisha, Jerdel, Boris, Pritheev
+
+# Imports
+import csv
+
+with open('records.csv', mode='r') as recordsCSV:
+    records = csv.reader(recordsCSV)
+    for line in records:
+        print(line)
